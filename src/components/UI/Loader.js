c0fa2d89b40ae3f84ad9795544beeb8c0d1dc96d
Aneesh Singh Rajoriya <aneeshsinghrajoriya@gmail.com>
2024-07-07 20:26:53 +0530

    loader

--- src/components/UI/Loader.js
+++ src/components/UI/Loader.js
@@ -3,11 +3,7 @@ import "./Loader.css"
 const Loader = () => {
   return (
         <div className="Loader">
-                    <div class="three-body">
-        <div class="three-body__dot"></div>
-        <div class="three-body__dot"></div>
-        <div class="three-body__dot"></div>
-        </div>
+               <div class="loader"></div>
         </div>
   )
 }
